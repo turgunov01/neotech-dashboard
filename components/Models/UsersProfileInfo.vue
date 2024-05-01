8fb85da96a1f345653a47b2a03276eca4e2c4181
Turgunov Sardor <sardorceeksamurai@gmail.com>
2024-05-01 12:30:00 +0500

    user -> history activity

---
+++ components/Models/UsersProfileInfo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="security">
+        <h1 class="security-title">Безопасность аккаунта</h1>
+        <div class="security-datas">
+            <div class="security-data">
+                <p class="security-data-name">Пользователь 1</p>
+                <div class="security-data-info">
+                    <p class="security-data-info-content"> {{ user.email }} </p>
+                    <img src="/assets/edit.svg" alt="">
+                </div>
+            </div>
+            <div class="security-data">
+                <p class="security-data-name">Пароль</p>
+                <div class="security-data-info">
+                    <p class="security-data-info-content"> {{ user.password }} </p>
+                    <img src="/assets/edit.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    user: {
+        required: true,
+        type: Object,
+        default: {
+            email: 'sardorceeksamurai@gmail.com',
+            phone: '+7 999 999 99 99',
+            password: '*********'
+        }
+    }
+})
+</script>
+
+<style scoped lang="scss"></style>
--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="info">
-        <div class="info-container"></div>
+        <div class="info-container">
+            <div class="user-parameters">
+                <UsersInfo />
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import UsersInfo from '~/components/Models/UsersProfileInfo.vue';
+
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
