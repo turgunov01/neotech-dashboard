bee7dada64d2eb5612ce1249a5309892bfe0793e
Turgunov Sardor <sardorceeksamurai@gmail.com>
2024-05-07 10:13:31 +0500

    dragNdrop api

--- components/Models/ConstructorSidebarElements.vue
+++ components/Models/ConstructorSidebarElements.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="aside-container">
-        
+        <div class="aside-buttons">
+            <button class="aside-button" :class="active === 0 ? 'active' : ''" @click="toggle(0)">
+                <p class="aside-button-name">Вставка</p>
+            </button>
+            <button class="aside-button" :class="active === 1 ? 'active' : ''" @click="toggle(1)">
+                <p class="aside-button-name">Страницы</p>
+            </button>
+        </div>
+        <div class="aside-content">
+            <div v-if="active === 0">
+                <ConstructorSidebarInsert />
+            </div>
+            <div v-else-if="active === 1">
+                <!-- <ConstructorSidebarPages /> -->
+                <p>1</p>
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script setup lang="ts">
+import ConstructorSidebarInsert from './ConstructorSidebarInsert.vue';
+
+const active = ref(0)
+
+const toggle = (index: Number) => {
+    (active as any).value = index
+}
 
 </script>
 
--- pages/constructor/index.vue
+++ pages/constructor/index.vue
@@ -1,14 +1,41 @@
 <template>
-    <aside class="aside">
-        <ConstructorSidebarElements />
-    </aside>
+    <div class="wrapper">
+        <aside class="aside">
+            <ConstructorSidebarElements />
+        </aside>
+        <div class="demo">
+            <div class="demo-container">
+                <div class="demo-page">
+                    <div class="demo-container">
+                        <TestComponent />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
-
+import TestComponent from '~/components/TestComponent.vue';
 import ConstructorSidebarElements from '../../components/Models/ConstructorSidebarElements.vue'
 
 </script>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+span {
+    color: #344054 !important;
+}
+
+
+.wrapper {
+    display: flex;
+}
+</style>
